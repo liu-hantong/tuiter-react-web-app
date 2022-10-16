03bb9e96d5982423973cb205fbfd43afd3cbab0c
Hantong Liu <hantongliu@Hantongs-MacBook-Pro.local>
2022-10-16 15:39:46 -0700

    a5 adjust home background color

--- public/tuiter/home/index.js
+++ public/tuiter/home/index.js
@@ -5,13 +5,13 @@ import HomeComponent from "../HomeScreen/HomeComponent.js";
 function exploreComponent() {
     $('#wd-explore').append(`
   <div class="row mt-2">
-   <div class="col-2 col-md-2 col-lg-1 col-xl-2 bg-black">
+   <div class="col-2 col-md-2 col-lg-1 col-xl-2 bg-body">
     ${NavigationSidebar('Home')}
    </div>
-   <div class="col-10 col-lg-7 col-xl-6 bg-black">
+   <div class="col-10 col-lg-7 col-xl-6 bg-body">
     ${HomeComponent()}
    </div>
-   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 bg-black">
+   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 bg-body">
     ${PostSummaryList()}
    </div>
   </div>
